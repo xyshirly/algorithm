216cbc0fba2e440a528f33cb6eb6aa4434d053f5
XY <1272273089@qq.com>
2020-01-25 22:02:13 +0800

    bubble sort

---
+++ sort-bubble.js
@@ -0,0 +1,34 @@
+// 冒泡排序
+function sortBubble(arr) {
+  const len = arr.length;
+  if (!arr || !len) return false;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len - 1; i++) {
+      for (var j = 0; j < len - 1 - i; j++) {
+          if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+              var temp = arr[j+1];        // 元素交换
+              arr[j+1] = arr[j];
+              arr[j] = temp;
+          }
+      }
+  }
+  return arr;
+}
+
+let arr = [3, 1, 4, 40, 30, 5, 20];
+console.log(bubbleSort(arr));
