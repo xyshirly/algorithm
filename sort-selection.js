d0fc2605800b4102e5201f46dae12cee35c7e720
xy <1272273089@qq.com>
2020-02-18 16:37:14 +0800

    sort selection is done

--- sort-selection.js
+++ sort-selection.js
@@ -1,9 +1,29 @@
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
+// 选择排序
+// 思路：从未排区间找到一个最小或者最大值，放在已排区间的末尾
+function sortSelection(arr) {
+  if (arr.length <= 1) return arr;
 
-    //从未排序区间中选择一个最小的元素
-    for (let j = 0; j < arr.length; j++) {
-      
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let j = i, min = arr[j], minIndex = j;
+
+    // 从未排区间中找到最小值和其位置
+    while (j < len) {
+      if (min > arr[j]) {
+        min = arr[j];
+        minIndex = j;
+      }
+      j++;
     }
+
+    // 把最小值放在已排区间的末尾
+    let temp = arr[i];
+    arr[i] = min;
+    arr[minIndex] = temp;
   }
+
+  return arr;
 }
+
+const arr = [9, 3, 1, 4, 2, 7];
+console.log(sortSelection(arr))
